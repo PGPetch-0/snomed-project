19f4dc605e0553826e20cb678ae29b9dcf773c3f
tyoohoon <57979899+tyoohoon@users.noreply.github.com>
2022-03-18 23:58:46 +0700

    enable cors

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors');
 
 const diagnosis = require('./routes/diagnosis')
 
 app.use(morgan('tiny'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+    origin: ['http://localhost:3000']
+}));
 
 app.use('/diagnosis', diagnosis)
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000 . . .')
 })
+
+
