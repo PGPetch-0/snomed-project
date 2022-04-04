2c274ad5652d6accecca448c2d24e26480aa12ee
tyoohoon <57979899+tyoohoon@users.noreply.github.com>
2022-04-04 16:47:09 +0700

    change mock diagnosisResult format

--- mockData/mockDiagnosis.js
+++ mockData/mockDiagnosis.js
@@ -4,33 +4,33 @@ const diagnosisResult = [
         diagnosis: 'กกกกกกกกกกก',
         probability: '80.00%',
         keyword: {
-            a: 'inactiveeeeee',
-            b: 'inactive',
-            c: 'inactive',
+            a: ['inactiveeeeee', 'inactiveeeeee', 'inactiveeeeee', 'inactiveeeeee', 'inactiveeeeee'],
+            b: ['inactive', 'inactive', 'inactive', 'inactive', 'inactive', 'inactive'],
+            c: ['inactive', 'inactive', 'inactive', 'inactive', 'inactive', 'inactive'],
         }
     }, {
         diagnosis: 'ขขขขขขขขขขข',
         probability: '75.00%',
         keyword: {
-            a: 'inactive',
-            b: 'inactive',
-            c: 'inactive',
+            a: ['inactiveeeeee', 'inactiveeeeee', 'inactiveeeeee', 'inactiveeeeee', 'inactiveeeeee'],
+            b: ['inactive', 'inactive', 'inactive', 'inactive', 'inactive', 'inactive'],
+            c: ['inactive', 'inactive', 'inactive', 'inactive', 'inactive', 'inactive'],
         }
     }, {
         diagnosis: 'คคคคคคคคคคคคคคคค',
         probability: '75.00%',
         keyword: {
-            a: 'inactive',
-            b: 'inactive',
-            c: 'inactive',
+            a: ['inactiveeeeee', 'inactiveeeeee', 'inactiveeeeee', 'inactiveeeeee', 'inactiveeeeee'],
+            b: ['inactive', 'inactive', 'inactive', 'inactive', 'inactive', 'inactive'],
+            c: ['inactive', 'inactive', 'inactive', 'inactive', 'inactive', 'inactive'],
         }
     }, {
         diagnosis: 'งงงงงงงงงง',
         probability: '60.50%',
         keyword: {
-            a: 'inactive',
-            b: 'inactive',
-            c: 'inactive',
+            a: ['inactiveeeeee', 'inactiveeeeee', 'inactiveeeeee', 'inactiveeeeee', 'inactiveeeeee'],
+            b: ['inactive', 'inactive', 'inactive', 'inactive', 'inactive', 'inactive'],
+            c: ['inactive', 'inactive', 'inactive', 'inactive', 'inactive', 'inactive'],
         }
     }
 ]
